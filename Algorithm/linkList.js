d3f449de945a922312f3beb40fc57ee76d7b6ae0
liuxinye <1170806961@qq.com>
2020-05-14 18:36:09 +0800

    滚动联系

---
+++ Algorithm/linkList.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: liuxinye
+ * @Date: 2020-05-01 14:35:44
+ * @LastEditors: liuxinye
+ * @LastEditTime: 2020-05-01 14:35:44
+ * @Description: 
+ */
---
+++ html/array/test_array.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: liuxinye
+ * @Date: 2020-05-06 10:33:49
+ * @LastEditors: liuxinye
+ * @LastEditTime: 2020-05-07 14:16:33
+ * @Description: 
+ */
+let arr=[1,2,3];
+res=arr.map((item)=>{
+    return item+1;
+})
+console.log(res)//[2,3,4];
