1ddc786573c7d64649b1fa8a4bdfade272d5d1ab
liuxinwei <1642825823@qq.com>
2020-09-08 19:55:25 +0800

    update

--- Algorithm/array.js
+++ Algorithm/array.js
@@ -1,12 +1,10 @@
-
-
 let matrix=[
     [0 , 1 , 2 ],
     [3 , 4 , 5 ],
     [6 , 7 , 8 ],
     [9 , 10, 11]
 ];
-// let matrix=[
+//  matrix=[
 //     [0 , 1 , 2 , 3],
 //     [4 , 5 , 6 , 7],
 //     [8 , 9 , 10, 11]
@@ -14,63 +12,49 @@ let matrix=[
 //1. 递增的维度
 //2. 方向的变化，什么时候该回转，某个方向上不能再递增或者递减时即为回转的时机（方向状态）
 //3. 回转时的值记录，回转时的值是本次遍历的起始依据
-let m=4,n=3;
+let m=matrix[0].length,n=matrix.length;
 let point={x:0,y:0};//用于记录回转时的值
 let addX=true;//0:x轴，1：y轴
 let turn=true;
 let res=[];
 let min=Math.min(m,n);
 while(turn){
-    if(point.x==0&&point.y==0){
-        res.push(matrix[0][0]);
-        point.y+=1;//{x:1,y:0}记录转向后起始的坐标点
-        continue;
+    if(addX){
+        let nowX=point.x,nowY=point.y;
+        while(nowX<=n-1&&nowY>=0){
+            point.x=nowX;
+            point.y=nowY;
+            res.push(matrix[nowX][nowY]);
+            nowX+=1;
+            nowY-=1;
+        }
+        addX=!addX;
+        if(point.x<n-1){
+            point.x+=1;
+        }else if(point.y<m-1){
+            point.y+=1;
+        }else{
+            //到头了
+            turn=false;
+        }
     }else{
-        try {
-            if(addX){
-                let nowX,nowY;
-                for(let y=0;y<=Math.min(point.y,min-1);y++){
-                    nowX=point.x+y;
-                    nowY=point.y-y;
-                    res.push(matrix[nowX][nowY])
-                }
-                addX=!addX;
-                point.x=nowX;
-                point.y=nowY;
-                if(point.x<n-1){
-                    point.x+=1;
-                }else if(point.y<m-1){
-                    point.y+=1;
-                }else{
-                    //到头了
-                    turn=false;
-                }
-            }else{
-                let nowX,nowY;
-                for(let x=0;x<=Math.min(point.x,min-1);x++){
-                    nowX=point.x-x;
-                    nowY=point.y+x;
-                    res.push(matrix[nowX][nowY])
-                }
-                point.x=nowX;
-                point.y=nowY;
-                addX=!addX;
-                if(point.y<m-1){
-                    point.y+=1;
-                }else if(point.x<n-1){
-                    point.x+=1;
-                }else{
-                    //到头了
-                    turn=false;
-                }
-            }
-        } catch (error) {
-            // console.log(error)
-            console.log(point)
-            console.log(res)
+        let nowX=point.x,nowY=point.y;
+        while(nowX>=0&&nowY<=m-1){
+            point.x=nowX;
+            point.y=nowY;
+            res.push(matrix[nowX][nowY]);
+            nowX-=1;
+            nowY+=1;
+        }
+        addX=!addX;
+        if(point.y<m-1){
+            point.y+=1;
+        }else if(point.x<n-1){
+            point.x+=1;
+        }else{
+            //到头了
             turn=false;
         }
     }
-
-
 }
+console.log(res);
